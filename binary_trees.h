50a649ad37711f6980e7baaa15e86ac7e6249c85
Idowu-David <idowudavudodun@gmail.com>
2023-03-31 04:07:06 +0000

    add: is leaf?

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,11 @@ void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+
+
+
 
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
