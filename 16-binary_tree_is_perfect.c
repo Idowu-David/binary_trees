97e528134e5a18a10863b822c9165c46a24d0101
David Idowu <idowudavidodun@gmail.com>
2025-07-19 01:01:00 +0100

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -4,17 +4,45 @@
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: a pointer to the root node of the tree to check
  * Return: 1 if perfect, 0, otherwise
- * A perfect Binary Tree has all leaf nodes on the same level, which means that all levels are full of nodes,
- * and all internal nodes have two child nodes.
- * The properties of a perfect Binary Tree means it is also full, balanced, and complete.
+ * A perfect binary tree has:
+ * - All levels completely filled
+ * - All leaf nodes at the same depth
+ * - All internal nodes have two children
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int dep;
+
+	if (!tree)
+		return (0);
+
+	dep = depth(tree);
+	return (is_perfect_helper(tree, dep, 1));
+}
+
+/**
+ * depth - measures the depth of the leftmost leaf node
+ * @tree: Pointer to the root node of the tree
+ * Return: Depth of the leftmost path
+ */
+int depth(const binary_tree_t *tree)
+{
+	int depth = 0;
+	while (tree)
+	{
+		depth++;
+		tree = tree->left;
+	}
+	return (depth);
+}
+
+int is_perfect_helper(const binary_tree_t *tree, int depth, int level)
+{
 	if (tree == NULL)
-		return (0);
-	if (!tree->left || !tree->right)
-		return (0);
-	if ((!tree->left && !tree->right))
 		return (1);
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	if (!tree->left && !tree->right) /* Leaf node check */
+		return (depth == level);
+	if (!tree->left || !tree->right) /* One child is missing */
+		return (0);
+	return (is_perfect_helper(tree->left, depth, level + 1) && is_perfect_helper(tree->right, depth, level + 1));
 }
